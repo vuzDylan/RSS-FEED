fd8b3166d371f8be54718d350f0f4cc646103b9e
Dylan Vuz <vuzdylan@gmail.com>
2015-11-14 22:10:19 -0500

    Pull Boiler plate

---
+++ app/js/components/App.jsx
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
+
+export default App;
